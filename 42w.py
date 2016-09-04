8bcbb4d3e28f7c3161c64c1e05dd363e8719e3a3
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-09-03 21:12:16 -0400

    Update 42w.py Version 0.1.1
    Removido os upper() desnecessarios!

--- 42w.py
+++ 42w.py
@@ -4,7 +4,7 @@
 # https://github.com/EdwardRamos2/42w
 
 print ("42w")
-print ("Version 0.1.0")
+print ("Version 0.1.1")
 print ("Edward Ramos")
 print ("https://github.com/EdwardRamos2/42w/")
 import os
@@ -12,24 +12,24 @@ import os
 print ("MENU")
 opcao1 = (raw_input("ENTER (1) - PARA VERFICAR INTERFACES:  "))
 
-if opcao1.upper() == '1':
+if opcao1 == '1':
  onl = os.system("iw dev")
  print onl
 
 
 opcao2 = (raw_input("ENTER (2) - check PROCESSOS ATIVOS:  "))
-if opcao2.upper() == '2':
+if opcao2 == '2':
 	fina = os.system("airmon-ng check")
 	print fina
 	
 opcao3 = (raw_input("ENTER (3) - kill PROCESSOS ATIVOS:  "))
-if opcao3.upper() == '3':
+if opcao3 == '3':
 	morte = os.system("airmon-ng check kill")	
 	print morte
 
 opcao4 = (raw_input("DIGITE NOME SUA  INTERFACE: "))
 print ("wlan1 e wlan0 aceitos")
-#face = (raw_input("ENTER NOME DE SUA INTERFACE"))
+
 if opcao4 == 'wlan1':
 	uppp = os.system("ifconfig wlan1 up")
 	print uppp
@@ -42,7 +42,7 @@ print listar2
 
 sair = (raw_input("ENTER (5) - PARA SAIR:  "))
  
-if sair.upper() == '5':
+if sair == '5':
 	print ("Saindo em 3.2.1")
 	exit
 else:
