b03dd52c3369a3823502be9e6ac18260f432fdd8
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-11-05 22:54:49 -0200

    Update 42w.py

--- 42w.py
+++ 42w.py
@@ -4,50 +4,59 @@
 # https://github.com/EdwardRamos2/42w
 
 print ("42w")
-print ("Version 0.1.1")
+print ("Version 0.1.1  NEW Version 0.2.1")
 print ("Edward Ramos")
 print ("https://github.com/EdwardRamos2/42w/")
+
+
+
+#!/usr/bin/env python
+
+
+import sys
 import os
+"""
+def menu_opcoes():
+    menu_wireless = open('menu.txt')
+    print(menu_wireless.read())  
+    opcao = input('')
+    if opcao == '1':
+        os.system('ifconfig')
+    elif opcao == '2':
+        print('Saindo em 3...');print('2..');print('1.')
+        sys.exit(1)
+menu_opcoes()
 
-print ("MENU")
-opcao1 = (raw_input("ENTER (1) - PARA VERFICAR INTERFACES:  "))
+def download_interface():
+    download_faces = open('interface.txt')
+    print(download_faces.read())
+         
+    opcao_interface = input('(+) Interface: ')
+    if opcao_interface == '1':
+        print('(+) Interface escolhida: (wlan0) Opcao: %s' % opcao_interface)
+        os.system('ifconfig wlan0 down ')
+    elif opcao_interface == '2':
+        print('(+) Interface escolhida: (wlan1) Opcao: %s' % opcao_interface)
+        os.system('ifconfig wlan1 down')
+    elif opcao_interface == '3':
+        print('(+) Interface escolhida: (wlp3s0) Opcao: %s' % opcao_interface)
+        os.system('ifconfig wlp3s0 down')
+    elif opcao_interface == '4':
+        print('(+) Interface escolhida: (wlp0s20u1) Opcao: %s' % opcao_interface)
+        os.system('ifconfig wlp0s20u1 down')
+download_interface()
+"""
 
-if opcao1 == '1':
- onl = os.system("iw dev")
- print onl
-
-
-opcao2 = (raw_input("ENTER (2) - check PROCESSOS ATIVOS:  "))
-if opcao2 == '2':
-	fina = os.system("airmon-ng check")
-	print fina
-	
-opcao3 = (raw_input("ENTER (3) - kill PROCESSOS ATIVOS:  "))
-if opcao3 == '3':
-	morte = os.system("airmon-ng check kill")	
-	print morte
-
-opcao4 = (raw_input("DIGITE NOME SUA  INTERFACE: "))
-print ("wlan1 e wlan0 aceitos")
-
-if opcao4 == 'wlan1':
-	uppp = os.system("ifconfig wlan1 up")
-	print uppp
-elif opcao4 == 'wlan0':
-	uppp2 = os.system("ifconfig wlan0 up")
-	print uppp2	
-
-listar2 = os.system("ifconfig")	
-print listar2
-
-sair = (raw_input("ENTER (5) - PARA SAIR:  "))
- 
-if sair == '5':
-	print ("Saindo em 3.2.1")
-	exit
-else:
-	print ("error, digite o valor solicitado :)")
-
-
+def tipo_attack():
+    opcao_attack = open('tipo_attacks.txt')
+    print(opcao_attack.read())
+    mac_alvo = input('Digite o MAC Router Destino: Exemplo: 54:B8:0A:F3:3F:C3 >>>  ')
+    mac_usuario = input('Digite o MAC Cliente Router Destino: Exemplo: B8:5A:73:AC:60:CC >>>  ')
+    delimiter = ':'
+    mac_alvo = mac_alvo.split(delimiter)
+    print(mac_alvo)
+    #if opcao_attack == '-0':
+   #     os.system()
+tipo_attack()
 
 
